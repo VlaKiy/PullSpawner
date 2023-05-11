9277e3d010770dbd4efe6d322974f7cac92991e6
Vlad Bistreckiy <vlad.zero07@mail.ru>
2023-05-11 13:18:58 +0500

    init

---
+++ Assets/Scripts/InputSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSpawn : MonoBehaviour
+{
+    [SerializeField] private Spawner _spawner;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject randomPrefab = _spawner.GetRandomPrefab();
+
+            _spawner.Spawn(randomPrefab);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            List<GameObject> objects = _spawner.SpawnedObjects;
+
+            int randomIndex = Random.Range(0, objects.Count);
+
+            objects[randomIndex].SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private Collider _collider => GetComponent<BoxCollider>();
+
+    public Vector3 BoundsMin => _collider.bounds.min;
+    public Vector3 BoundsMax => _collider.bounds.max;
+}
